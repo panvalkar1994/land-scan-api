ee80e2e2d4e9856e5aa3cc9c127af15a718569dd
vaibhav <vpanvalkar@outlook.com>
2022-01-02 15:23:30 +0530

    auth

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -5,7 +5,9 @@ export function auth(req:Request, res:Response, next:NextFunction){
     const authHeader  = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if(!token){
-        return res.status(401)
+        return res.status(401).send({
+            message:"UnAuthorized"
+        });
     }
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, userId)=>{
         if(err) return res.status(403).send();
--- src/routes/region.router.ts
+++ src/routes/region.router.ts
@@ -5,7 +5,7 @@ import { auth } from "../middleware/auth.middleware";
 const router = Router();
 
 // router.get('/:id', getRegion);
-router.post("/create", createRegionHandler);
+router.post("/create", auth, createRegionHandler);
 // router.post('/update/:id', auth, updateRegion);
 // router.delete('/:id',auth, deleteRegion);
 
